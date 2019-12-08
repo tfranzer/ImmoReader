675abe12c72135cd07e513712d582bdcc5c86aac
Torsten Franz <tfranzer@gmx.de>
2019-12-08 11:31:11 +0100

    Projektdateien hinzufügen.

---
+++ ImmoReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImmoReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
